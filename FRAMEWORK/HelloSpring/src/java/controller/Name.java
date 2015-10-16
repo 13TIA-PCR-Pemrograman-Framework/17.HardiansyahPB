4c82c1b6ea99fe6992bbaf8077817e5e9b8092d9
LabGSG <LabGSG@LabGSG-PC2a>
2015-10-16 07:58:25 +0700

    HelloSpring

---
+++ FRAMEWORK/HelloSpring/src/java/controller/Name.java
@@ -0,0 +1,26 @@
+package controller;
+
+/**
+ *
+ * @author nbuser
+ */
+public class Name {
+
+    private String value, age;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
---
+++ FRAMEWORK/HelloSpring/src/java/service/HelloService.java
@@ -0,0 +1,12 @@
+package service;
+
+/**
+ *
+ * @author nbuser
+ */
+public class HelloService {
+
+    public String sayHello(String name, String age) {
+        return "Hello " + name + "! Your age is " + age;
+    }
+}
